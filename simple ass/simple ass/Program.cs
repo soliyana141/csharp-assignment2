296c64f72586c4fc3c66a7dc348b8a5b29888812
soliyana141 <107803793+soliyana141@users.noreply.github.com>
2022-06-26 13:49:23 +0300

    second commit even and odd counter

---
+++ simple ass/simple ass/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int size, even, odd;
+        int[] arr;
+
+        
+        Console.Write("Enter size of the array: ");
+        size = Convert.ToInt32(Console.ReadLine());
+
+        
+        Console.WriteLine("Enter {0} elements in array:", size);
+
+        arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("Number({0}) :", i + 1);
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+       
+        even = 0;
+        odd = 0;
+        for (int i = 0; i < size; i++)
+        {
+            
+            if (arr[i] % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+        Console.Write("Total even elements: {0}\n", even);
+        Console.Write("Total odd elements: {0}", odd);
+
+        Console.ReadLine();
+    }
+}
+
---
+++ simple/simple/Program.cs
@@ -0,0 +1,25 @@
+﻿static int ReadInput(string message)
+{
+    int n = 0;
+    do
+    {
+        Console.WriteLine(message);
+    }
+    while (!int.TryParse(Console.ReadLine(), out n));
+
+    return n;
+}
+
+
+static void main(string[]args)
+{
+    int i = ReadInput("Enter a Number");
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("even");
+    }
+    else if (i % 2 != 0)
+    {
+        Console.WriteLine("odd");
+    }
+}
